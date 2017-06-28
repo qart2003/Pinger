5815751b9d29c32b4958cd6602e64f657c1f699f
qart2003 <qart2003@rambler.ru>
2017-06-28 14:54:12 +0300

    Test

---
+++ Pinger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinger
+{
+  class Program
+  {
+    static private void PingAll(List<string> ips)
+    {
+      ips.AsParallel().Select(ip => { Console.WriteLine("Try " + ip); Ping ping = new Ping(); ping.PingCompleted += Ping_PingCompleted; ping.SendAsync(ip, 2000);  return string.Empty; } );
+    }
+    static private void Ping_PingCompleted(object sender, PingCompletedEventArgs e)
+    {
+      if (e.Error == null && e.Reply != null)
+        Console.WriteLine(e.Reply.Address.ToString() + " - Ok!");
+      else Console.WriteLine(e.Reply.Address.ToString() + " - Bad!");
+    }
+
+    static void Main(string[] args)
+    {
+      List<string> ips = new List<string>();
+      for (int i = 45; i < 50; i++)
+      {
+        ips.Add("109.252.44." + i.ToString());
+      }
+      PingAll(ips);
+      Console.ReadKey();
+    }
+  }
+}
